985c6f0b8efaf6c46d948bd3f878363955eef586
marc.portabella <marc.portabella@tlgcommerce.com>
2019-10-03 12:55:47 +0200

    startup

--- serverlessEnvStartup.sh
+++ serverlessEnvStartup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Install dependencies
-npm install serverless
-npm install serverless-python-requirements
-apt install -y virtualenv
+# Set AWS
+source aws.properties
+sed -i '/export AWS_PROFILE/d' ~/.bashrc
+sed -i '/export AWS_PROFILE/d' /etc/profile
+echo "export AWS_PROFILE=izhan && export AWS_REGION=eu-west-1" >> ~/.bashrc
+echo "export AWS_PROFILE=izhan && export AWS_REGION=eu-west-1" >> /etc/profile
 
 # Run virutal environment
 virtualenv -p python3 venv
 source venv/bin/activate
+
+# Install dependencies
+npm install -g serverless
+npm install -g serverless-python-requirements
+apt install -y virtualenv
 
 # Install PIP inside the env
 pip install -r requirements.txt
